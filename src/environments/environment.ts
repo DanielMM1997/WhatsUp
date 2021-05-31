fc6f8397d872f0705343b3dc8ec9cb3f54b4f272
Daniel Martinez <daniel_abeto@hotmail.es>
2021-05-31 17:08:10 +0100

    add production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: "AIzaSyBCqwYOQW8ZaFo2fxRTvv2ggGZfEZASynY",
     authDomain: "whatsup-80e9a.firebaseapp.com",
